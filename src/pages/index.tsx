2d87cd395382d8b6ed4eaf8d87ea1c793b0e5718
Devin Lee <ukjae29@gmail.com>
2024-02-26 15:44:14 +0900

    Feat: 시작페이지 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,162 @@ const Home = () => {
     return (
         <Box>
             <HeaderBar />
+            <Box
+                sx={{
+                    marginTop: 10,
+                    height: '30vh',
+                    '@media (min-width: 600px)': {
+                        width: '100vw',
+                        height: '45vh',
+                    },
+                    '@media (min-width: 900px)': {
+                        width: '100vw',
+                        height: '40vh',
+                    },
+                    '@media (min-width: 1100px)': {
+                        width: '100vw',
+                        height: '50vh',
+                    },
+                    '@media (min-width: 1400px)': {
+                        width: '100vw',
+                        height: '60vh',
+                    },
+                    '@media (min-width: 1600px)': {
+                        width: '100vw',
+                        height: '80vh',
+                    },
+                    '@media (min-width: 1800px)': {
+                        width: '100vw',
+                        height: '100vh',
+                    },
+                    '& img': {
+                        filter: 'none',
+                        transition: 'filter 0.3s ease',
+                        '&:hover': {
+                            filter: 'grayscale(100%)',
+                        },
+                    },
+                }}
+            >
+                <img src="src/assets/images/cafe-view.jpg" width="100%" height="100%" alt="cafe-view" />
+            </Box>
+            <Box
+                sx={{
+                    height: '10vh',
+                }}
+            ></Box>
+            <Box
+                sx={{
+                    height: '0vh',
+                    '@media (min-width: 900px)': {
+                        width: '100vw',
+                        height: '40vh',
+                    },
+                    '@media (min-width: 1100px)': {
+                        width: '100vw',
+                        height: '50vh',
+                    },
+                    '@media (min-width: 1400px)': {
+                        width: '100vw',
+                        height: '60vh',
+                    },
+                    '@media (min-width: 1600px)': {
+                        width: '100vw',
+                        height: '80vh',
+                    },
+                    '@media (min-width: 1800px)': {
+                        width: '100vw',
+                        height: '90vh',
+                    },
+                    '& img': {
+                        filter: 'none',
+                        transition: 'filter 0.3s ease',
+                        '&:hover': {
+                            filter: 'grayscale(100%)',
+                        },
+                    },
+                }}
+            >
+                <img src="src/assets/images/coffee-drip.jpg" width="100%" height="100%" alt="coffee-drip" />
+            </Box>
+            <Box
+                sx={{
+                    height: '10vh',
+                }}
+            ></Box>
+            <Box
+                sx={{
+                    height: '0vh',
+                    '@media (min-width: 900px)': {
+                        width: '100vw',
+                        height: '40vh',
+                    },
+                    '@media (min-width: 1100px)': {
+                        width: '100vw',
+                        height: '50vh',
+                    },
+                    '@media (min-width: 1400px)': {
+                        width: '100vw',
+                        height: '60vh',
+                    },
+                    '@media (min-width: 1600px)': {
+                        width: '100vw',
+                        height: '80vh',
+                    },
+                    '@media (min-width: 1800px)': {
+                        width: '100vw',
+                        height: '90vh',
+                    },
+                    '& img': {
+                        filter: 'none',
+                        transition: 'filter 0.3s ease',
+                        '&:hover': {
+                            filter: 'grayscale(100%)',
+                        },
+                    },
+                }}
+            >
+                <img src="src/assets/images/coffee-roasting.jpg" width="100%" height="100%" alt="coffee-roasting" />
+            </Box>
+            <Box
+                sx={{
+                    height: '10vh',
+                }}
+            ></Box>
+            <Box
+                sx={{
+                    height: '0vh',
+                    '@media (min-width: 900px)': {
+                        width: '100vw',
+                        height: '40vh',
+                    },
+                    '@media (min-width: 1100px)': {
+                        width: '100vw',
+                        height: '50vh',
+                    },
+                    '@media (min-width: 1400px)': {
+                        width: '100vw',
+                        height: '60vh',
+                    },
+                    '@media (min-width: 1600px)': {
+                        width: '100vw',
+                        height: '80vh',
+                    },
+                    '@media (min-width: 1800px)': {
+                        width: '100vw',
+                        height: '90vh',
+                    },
+                    '& img': {
+                        filter: 'none',
+                        transition: 'filter 0.3s ease',
+                        '&:hover': {
+                            filter: 'grayscale(100%)',
+                        },
+                    },
+                }}
+            >
+                <img src="src/assets/images/coffee-brewing.jpg" width="100%" height="100%" alt="coffee-brewing" />
+            </Box>
         </Box>
     );
 };
