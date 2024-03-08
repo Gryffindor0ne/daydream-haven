35672758fe377c4d1c15669f0009abac69b8f382
Devin Lee <ukjae29@gmail.com>
2024-03-08 16:45:38 +0900

    Feat: 리덕스 직렬화 관련 에러 처리 설정

--- src/app/store.ts
+++ src/app/store.ts
@@ -19,6 +19,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
