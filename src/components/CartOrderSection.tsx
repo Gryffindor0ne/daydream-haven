912b91680dd23883776424534035d54571a3e551
Devin Lee <ukjae29@gmail.com>
2024-03-15 11:58:32 +0900

    Refactor: 카트주문섹션 컴포넌트로 분리

---
+++ src/components/CartOrderSection.tsx
@@ -0,0 +1,138 @@
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import DragHandleIcon from '@mui/icons-material/DragHandle';
+import { useAppSelector } from '~/app/reduxHooks';
+import { orderState } from '~/features/order/orderSlice';
+import { formattedNumber } from '~/utils/utils';
+
+const CartOrderSection = ({ products }: { products: string[] }) => {
+    const navigate = useNavigate();
+    const { deliveryFee, subTotal, totalAmount } = useAppSelector(orderState);
+
+    return (
+        <>
+            <Box sx={{ display: 'flex', flexDirection: 'column', py: 7 }}>
+                <Box sx={{ borderBottom: '1px solid gray' }}>
+                    <Typography
+                        variant="body2"
+                        sx={{ display: 'flex', py: 2 }}
+                    >{`총 주문 수량 ${products.length} 개`}</Typography>
+                </Box>
+
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        py: 5,
+                        borderBottom: '1px solid gray',
+                    }}
+                >
+                    <Box>
+                        <Typography
+                            variant="subtitle1"
+                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
+                        >
+                            {products.length !== 0 ? formattedNumber(subTotal) : 0} 원
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5, color: '#B99470' }}
+                        >
+                            상품금액
+                        </Typography>
+                    </Box>
+
+                    <Typography variant="subtitle1" sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}>
+                        <AddOutlinedIcon />
+                    </Typography>
+                    <Box>
+                        <Typography
+                            variant="subtitle1"
+                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
+                        >
+                            {products.length !== 0 ? (deliveryFee ? '3,000원' : '무료') : '0 원'}
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5, color: '#B99470' }}
+                        >
+                            배송비
+                        </Typography>
+                    </Box>
+                    <Typography variant="subtitle1" sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}>
+                        <DragHandleIcon />
+                    </Typography>
+
+                    <Box>
+                        <Typography
+                            variant="subtitle1"
+                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
+                        >
+                            {products.length !== 0 ? formattedNumber(totalAmount) : 0} 원
+                        </Typography>
+                        <Typography
+                            variant="subtitle1"
+                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5, color: '#B99470' }}
+                        >
+                            총 주문금액
+                        </Typography>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    py: 5,
+                }}
+            >
+                {products.length !== 0 && (
+                    <Button
+                        onClick={() => navigate(`/order`)}
+                        variant="outlined"
+                        sx={{
+                            width: 200,
+                            height: 50,
+                            fontSize: 16,
+                            color: '#ffffff',
+                            background: '#B67352',
+
+                            '&:hover': {
+                                color: '#ffffff',
+                                background: '#B67352',
+                            },
+                        }}
+                    >
+                        주문하기
+                    </Button>
+                )}
+                <Box sx={{ py: 2 }}>
+                    <Button
+                        onClick={() => navigate(`/shop`)}
+                        variant="outlined"
+                        sx={{
+                            width: 200,
+                            height: 50,
+                            fontSize: 16,
+                            '&:hover': {
+                                color: '#B67352',
+                                background: '#ffffff',
+                            },
+                        }}
+                    >
+                        계속 쇼핑하기
+                    </Button>
+                </Box>
+            </Box>
+        </>
+    );
+};
+
+export default CartOrderSection;
--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,29 +1,23 @@
+import { useEffect, useState } from 'react';
+
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
-import DragHandleIcon from '@mui/icons-material/DragHandle';
+import { IconButton } from '@mui/material';
 
 import { useAppDispatch, useAppSelector } from '~/app/reduxHooks';
 import { cartState, removeFromCart, updateCartTotal } from '~/features/cart/cartSlice';
 import CartItem from '~/components/CartItem';
-import { useEffect, useState } from 'react';
-import { IconButton } from '@mui/material';
-import { addToOrder, orderState, updateOrderTotal } from '~/features/order/orderSlice';
-import { formattedNumber } from '~/utils/utils';
-import { useNavigate } from 'react-router-dom';
+import { addToOrder, updateOrderTotal } from '~/features/order/orderSlice';
+import CartOrderSection from '~/components/CartOrderSection';
 
 const ShoppingCart = () => {
     const { cartItems, deliveryFeeCondition } = useAppSelector(cartState);
-    const { deliveryFee, subTotal, totalAmount } = useAppSelector(orderState);
     const dispatch = useAppDispatch();
-
     const [allChecked, setAllChecked] = useState(false);
-
-    const navigate = useNavigate();
 
     //id값 모음
     const [selectedProducts, setSelectedProducts] = useState<string[]>([]);
@@ -89,7 +83,7 @@ const ShoppingCart = () => {
                         </Typography>
                     </Box>
                 </Box>
-                <Box sx={{ marginBottom: 2 }}>
+                <Box sx={{ marginBottom: 2, borderBottom: '1px solid #F4EDCC' }}>
                     {cartItems.length !== 0 ? (
                         cartItems.map((cartItem) => (
                             <CartItem
@@ -112,7 +106,7 @@ const ShoppingCart = () => {
                         </Box>
                     )}
                 </Box>
-
+                {/* 카트 관련 버튼 */}
                 {cartItems.length !== 0 && (
                     <Button
                         onClick={handleDeleteCartItems}
@@ -130,129 +124,8 @@ const ShoppingCart = () => {
                     </Button>
                 )}
 
-                <Box sx={{ display: 'flex', flexDirection: 'column', py: 7 }}>
-                    <Box sx={{ borderBottom: '1px solid gray' }}>
-                        <Typography
-                            variant="body2"
-                            sx={{ display: 'flex', py: 2 }}
-                        >{`총 주문 수량 ${selectedProducts.length} 개`}</Typography>
-                    </Box>
-
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'center',
-                            py: 5,
-                            borderBottom: '1px solid gray',
-                        }}
-                    >
-                        <Box>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
-                            >
-                                {selectedProducts.length !== 0 ? formattedNumber(subTotal) : 0} 원
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5, color: '#B99470' }}
-                            >
-                                상품금액
-                            </Typography>
-                        </Box>
-
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
-                        >
-                            <AddOutlinedIcon />
-                        </Typography>
-                        <Box>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
-                            >
-                                {selectedProducts.length !== 0 ? (deliveryFee ? '3,000원' : '무료') : '0 원'}
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5, color: '#B99470' }}
-                            >
-                                배송비
-                            </Typography>
-                        </Box>
-                        <Typography
-                            variant="subtitle1"
-                            sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
-                        >
-                            <DragHandleIcon />
-                        </Typography>
-
-                        <Box>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5 }}
-                            >
-                                {selectedProducts.length !== 0 ? formattedNumber(totalAmount) : 0} 원
-                            </Typography>
-                            <Typography
-                                variant="subtitle1"
-                                sx={{ display: 'flex', justifyContent: 'center', py: 2, px: 5, color: '#B99470' }}
-                            >
-                                총 주문금액
-                            </Typography>
-                        </Box>
-                    </Box>
-                </Box>
-
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        py: 5,
-                    }}
-                >
-                    {selectedProducts.length !== 0 && (
-                        <Button
-                            onClick={() => navigate(`/order`)}
-                            variant="outlined"
-                            sx={{
-                                width: 200,
-                                height: 50,
-                                fontSize: 16,
-                                color: '#ffffff',
-                                background: '#B67352',
-
-                                '&:hover': {
-                                    color: '#ffffff',
-                                    background: '#B67352',
-                                },
-                            }}
-                        >
-                            주문하기
-                        </Button>
-                    )}
-                    <Box sx={{ py: 2 }}>
-                        <Button
-                            onClick={() => navigate(`/shop`)}
-                            variant="outlined"
-                            sx={{
-                                width: 200,
-                                height: 50,
-                                fontSize: 16,
-                                '&:hover': {
-                                    color: '#B67352',
-                                    background: '#ffffff',
-                                },
-                            }}
-                        >
-                            계속 쇼핑하기
-                        </Button>
-                    </Box>
-                </Box>
+                {/* 카트 주문 관련 섹션 */}
+                {cartItems.length !== 0 && <CartOrderSection products={selectedProducts} />}
             </Box>
         </Container>
     );
