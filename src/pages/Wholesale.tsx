0dec71775ef1ce6fb0971e8608e8e77a140c96a7
Devin Lee <ukjae29@gmail.com>
2024-03-21 17:09:59 +0900

    Feat: 도매페이지 안내팝업 및 메인페이지 리다이렉트 구현(추후 구현예정)

--- src/pages/Wholesale.tsx
+++ src/pages/Wholesale.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import BasicAlert from '~/components/BasicAlert';
+import { useNavigate } from 'react-router-dom';
 
 const Wholesale = () => {
+    const [showAlert, setShowAlert] = useState<boolean>(false);
+    const [alertMessage, setAlertMessage] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleAlertClose = () => {
+        setShowAlert(false);
+        navigate('/');
+    };
+
+    useEffect(() => {
+        setShowAlert(true);
+        setAlertMessage('시업자 회원 전용 메뉴입니다.');
+    }, []);
+
     return (
         <Container maxWidth="lg">
-            <Box sx={{ minHeight: '75vh', paddingTop: 12, marginTop: 10 }}>
-                <Typography>Wholesale</Typography>
-            </Box>
+            {showAlert && <BasicAlert open={showAlert} onClose={handleAlertClose} message={alertMessage} />}
+            <Box sx={{ minHeight: '75vh', paddingTop: 12, marginTop: 10 }}></Box>
         </Container>
     );
 };
