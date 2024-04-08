1e5f60f16076bee86ec027f9ea34275165cbfd6b
Devin Lee <ukjae29@gmail.com>
2024-04-08 11:52:16 +0900

    Refactor: 주문리스트 없을시 주문페이지 접근 차단

--- src/pages/PrivatedRoute.tsx
+++ src/pages/PrivatedRoute.tsx
@@ -1,15 +1,20 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAppSelector } from '~/app/reduxHooks';
 import { authState } from '~/features/auth/authSlice';
+import { orderState } from '~/features/order/orderSlice';
 
 const PrivatedRoute = () => {
     const { isAuthenticated } = useAppSelector(authState);
+
+    const { orderItems } = useAppSelector(orderState);
     const currentLocation = useLocation();
 
     return isAuthenticated ? (
         <Outlet />
-    ) : (
+    ) : orderItems.length !== 0 ? (
         <Navigate to={'/login'} replace state={{ redirectedFrom: currentLocation }} />
+    ) : (
+        <Navigate to={`/`} />
     );
 };
 export default PrivatedRoute;
