10c90780358881f057da8859ce81b3f8eaaf3136
Devin Lee <ukjae29@gmail.com>
2024-10-26 19:42:42 +0900

    Fix: 로그아웃 오류 수정 위해  authSaga 로직 수정

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects';
-import { watchLoginWithToken, watchLogoutUser } from '~/features/auth/authSaga';
+import { watchCheckTokenExpiration, watchLogoutUser } from '~/features/auth/authSaga';
 import paymentSaga from '~/features/payment/paymentSaga';
 
 export default function* rootSaga() {
-    yield all([watchLoginWithToken(), watchLogoutUser(), paymentSaga()]);
+    yield all([watchCheckTokenExpiration(), watchLogoutUser(), paymentSaga()]);
 }
