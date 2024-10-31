544c0bc7dbcb1b6cbf74f4c185f0c98ac88e2098
Devin Lee <ukjae29@gmail.com>
2024-10-31 17:53:38 +0900

    Refactor: Login 페이지 UI 변경 및 코드 수정 &  로그인 API 생성

---
+++ src/api/getLoginTokenAPI.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { axiosInstance } from '~/lib/axiosInstance';
+import { LoginInfo } from '~/pages/auth/Login';
+
+const getLoginToken = async ({ email, password }: LoginInfo) => {
+    try {
+        const response = await axiosInstance.post(`users/sign-in`, {
+            email,
+            password,
+        });
+        return response;
+    } catch (error) {
+        const message =
+            axios.isAxiosError(error) && error.response
+                ? error.response.status === 401 // 예: 잘못된 로그인 정보
+                    ? '이메일 또는 비밀번호가 올바르지 않습니다.'
+                    : error.response.status === 404 // 예: 경로가 잘못된 경우
+                      ? '서버에 연결할 수 없습니다.'
+                      : '로그인 요청 중 오류가 발생했습니다.'
+                : '네트워크 오류가 발생했습니다.';
+
+        throw new Error(message);
+    }
+};
+
+export default getLoginToken;
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -8,28 +8,33 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-import { axiosInstance } from '~/lib/axiosInstance';
 import BasicPopup from '~/components/layout/popup/BasicPopup';
 import { useAppDispatch } from '~/app/reduxHooks';
 import { setAuthenticated, setLoading } from '~/features/auth/authSlice';
 import { setAccessTokenCookie } from '~/utils/cookiesUtils';
 import useScrollToTop from '~/hooks/useScrollToTop';
+import getLoginToken from '~/api/getLoginTokenAPI';
 
 const loginSchema = Yup.object().shape({
     email: Yup.string().email('유효한 이메일 주소를 입력하세요.').required('이메일을 입력해주세요.'),
     password: Yup.string().required('비밀번호를 입력해주세요.'),
 });
 
-type LoginInfo = {
+export type LoginInfo = {
     email: string;
     password: string;
 };
 
 const Login = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [message, setMessage] = useState<string>('');
 
@@ -40,53 +45,35 @@ const Login = () => {
 
     const queryPath = location?.search?.replace('?redirectedFrom=', '/');
     const from = queryPath || location?.state?.redirectedFrom?.pathname || '/';
+    const errorMessage = '이메일, 비밀번호가 맞지 않습니다. 다시 시도해주세요.';
 
-    const handleSubmit = async (values: LoginInfo) => {
+    const buttonHeight = isMobile ? 45 : 60;
+    const buttonFontSize = 16;
+
+    const login = async (email: string, password: string) => {
         dispatch(setLoading(true));
         try {
-            const response = await axiosInstance.post(`users/sign-in`, {
-                email: values.email,
-                password: values.password,
-            });
+            const response = await getLoginToken({ email, password });
             if (response.status === 201) {
-                //엑세스토큰 쿠키에 저장
                 setAccessTokenCookie(response.data.access_token);
-                // 리덕스 상태 변환
                 dispatch(setAuthenticated(true));
-                // 리다이렉트 페이지로 이동
-
                 navigate(from);
-                dispatch(setLoading(false));
             }
         } catch (error) {
-            setMessage(`이메일, 비밀번호가 맞지 않습니다. 다시 시도해주세요.`);
+            setMessage(errorMessage);
             setIsOpen(true);
-            console.log(error);
-        } finally {
-            dispatch(setLoading(false)); // 비동기 작업 완료 후 항상 로딩 상태를 false로 설정합니다.
-        }
-    };
-
-    const handleGuestLogin = async () => {
-        dispatch(setLoading(true));
-
-        try {
-            const response = await axiosInstance.post(`users/sign-in`, {
-                email: `${import.meta.env.VITE_GUEST_EMAIL}`,
-                password: `${import.meta.env.VITE_GUEST_PASSWORD}`,
-            });
-            if (response.status === 201) {
-                setAccessTokenCookie(response.data.access_token);
-                dispatch(setAuthenticated(true));
-
-                navigate(from);
-                dispatch(setLoading(false));
-            }
-        } catch (error) {
             console.log(error);
         } finally {
             dispatch(setLoading(false));
         }
+    };
+
+    const handleSubmit = (values: LoginInfo) => {
+        login(values.email, values.password);
+    };
+
+    const handleGuestLogin = () => {
+        login(import.meta.env.VITE_GUEST_EMAIL, import.meta.env.VITE_GUEST_PASSWORD);
     };
 
     const handleClose = () => {
@@ -96,24 +83,24 @@ const Login = () => {
     useScrollToTop();
 
     return (
-        <Container maxWidth="xs">
+        <Container maxWidth="sm">
             {isOpen && <BasicPopup open={isOpen} onClose={handleClose} message={message} />}
 
-            <Box sx={{ minHeight: '100vh', paddingTop: 20, marginTop: 10, marginBottom: 5 }}>
+            <Box sx={{ minHeight: '100vh', pt: isMobile ? 5 : 20, mt: 10, mb: 5 }}>
                 <Grid container spacing={2} justifyContent="center" alignItems="center">
                     <Grid item xs={12}>
                         <Typography
                             sx={{
                                 fontFamily: 'Fraunces',
                                 fontWeight: 700,
-                                fontSize: 36,
+                                fontSize: isMobile ? 25 : 36,
                                 color: '#191919',
                                 marginBottom: 6,
                             }}
                             align="center"
                             gutterBottom
                         >
-                            Daydream Haven
+                            로그인
                         </Typography>
                         <Formik
                             initialValues={{ email: '', password: '' }}
@@ -130,6 +117,7 @@ const Login = () => {
                                                 label="이메일"
                                                 variant="outlined"
                                                 fullWidth
+                                                size={isMobile ? 'small' : 'large'}
                                                 error={errors.email && touched.email}
                                                 helperText={errors.email && touched.email ? errors.email : ''}
                                             />
@@ -142,6 +130,7 @@ const Login = () => {
                                                 label="비밀번호"
                                                 variant="outlined"
                                                 fullWidth
+                                                size={isMobile ? 'small' : 'large'}
                                                 error={errors.password && touched.password}
                                                 helperText={errors.password && touched.password ? errors.password : ''}
                                             />
@@ -153,8 +142,8 @@ const Login = () => {
                                                 color="primary"
                                                 fullWidth
                                                 sx={{
-                                                    height: 60,
-                                                    fontSize: 16,
+                                                    height: buttonHeight,
+                                                    fontSize: buttonFontSize,
                                                     '&:hover': {
                                                         color: '#ffffff',
                                                         background: '#B67352',
@@ -184,8 +173,8 @@ const Login = () => {
                                 color="primary"
                                 fullWidth
                                 sx={{
-                                    height: 60,
-                                    fontSize: 16,
+                                    height: buttonHeight,
+                                    fontSize: buttonFontSize,
                                     '&:hover': {
                                         color: '#B67352',
                                         background: '#ffffff',
@@ -199,8 +188,8 @@ const Login = () => {
                             <Typography
                                 onClick={handleGuestLogin}
                                 sx={{
-                                    fontWeight: 200,
-                                    fontSize: 17,
+                                    fontWeight: 400,
+                                    fontSize: 18,
                                     marginY: 7,
                                     textDecoration: 'underline',
                                     cursor: 'pointer',
